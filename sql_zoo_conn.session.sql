575551a44b8e3d51df9debccf7bdba917c9a4cf8
ayhem bouabid <bouabidayhem@gmail.com>
2023-01-25 15:07:39 +0300

    finish my first Tableau challenge

--- sql_zoo_conn.session.sql
+++
@@ -1,7 +0,0 @@
-SELECT departmentid, AVG(salary)
-FROM employee
-GROUP BY DEPARTMENTID;
-
-
-SELECT departmentid, name, salary, AGV(salary) OVER (PARTITION departmentid) as average_salary
-FROM employee;
